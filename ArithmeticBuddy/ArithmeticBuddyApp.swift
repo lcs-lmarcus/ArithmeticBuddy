340b942a39c031891cb87283f657c2876ab0b28b
Marcus Li <mmli@lcs.on.ca>
2025-01-18 10:07:30 -0500

    Initial Commit

---
+++ ArithmeticBuddy/ArithmeticBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticBuddyApp.swift
+//  ArithmeticBuddy
+//
+//  Created by Marcus Li on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
